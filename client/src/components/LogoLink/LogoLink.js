e3d4a07f746a22eb786431cb3c241b74e60652d7
Cuong Tran <nhoismee@gmail.com>
2024-04-09 01:50:52 -0700

    updated login code

--- client/src/components/LogoLink/LogoLink.js
+++ client/src/components/LogoLink/LogoLink.js
@@ -1,72 +1,129 @@
-import React, { useEffect } from 'react';
-import ProductItem from '../ProductItem/ProductItem';
-import { useStoreContext } from '../../utils/GlobalState';
-import { UPDATE_PRODUCTS } from '../../utils/actions';
-import { useQuery } from '@apollo/client';
-import { QUERY_PRODUCTS } from '../../utils/queries';
-import { idbPromise } from '../../utils/helpers';
-import spinner from '../../Assets/spinner.gif';
+import React, { useState } from 'react';
+import Auth from '../../utils/auth';
+import { useMutation } from '@apollo/client';
+import { LOGIN } from "../../utils/mutations";
+import { ADD_USER } from '../../utils/mutations';
+import {
+  MDBContainer,
+  MDBTabs,
+  MDBTabsItem,
+  MDBTabsLink,
+  MDBTabsContent,
+  MDBTabsPane,
+  MDBBtn,
+  MDBInput,
+  MDBCheckbox
+}
+from 'mdb-react-ui-kit';
 
-function ProductList() {
-  console.log("hi")
-  const [state, dispatch] = useStoreContext();
-  console.log("hi")
-  const { currentCategory } = state;
-  console.log("hi")
-  const { loading, data } = useQuery(QUERY_PRODUCTS);
-  console.log("hi")
+function Login(props) {
 
-  useEffect(() => {
-    if (data) {
-      dispatch({
-        type: UPDATE_PRODUCTS,
-        products: data.products,
-      });
-      data.products.forEach((product) => {
-        idbPromise('products', 'put', product);
-      });
-    } else if (!loading) {
-      idbPromise('products', 'get').then((products) => {
-        dispatch({
-          type: UPDATE_PRODUCTS,
-          products: products,
+  const [justifyActive, setJustifyActive] = useState('tab1');;
+
+  const handleJustifyClick = (value) => {
+    if (value === justifyActive) {
+      return;
+    }
+
+    setJustifyActive(value);
+  };
+
+
+    const [formState, setFormState] = useState({ email: '', password: '' });
+    const [login, { error }] = useMutation(LOGIN);
+    const [addUser] = useMutation(ADD_USER);
+
+    const handleFormLogin = async (event) => {
+      event.preventDefault();
+      try {
+        const mutationResponse = await login({
+          variables: { email: formState.email, password: formState.password },
         });
+        const token = mutationResponse.data.login.token;
+        Auth.login(token);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+  
+    const handleFormSignup = async (event) => {
+      event.preventDefault();
+      const mutationResponse = await addUser({
+      variables: {
+          email: formState.email,
+          password: formState.password,
+          firstName: formState.firstName,
+          lastName: formState.lastName,
+      },
+  });
+  const token = mutationResponse.data.addUser.token;
+  Auth.login(token);
+  };
+
+    const handleChange = (event) => {
+      const { name, value } = event.target;
+      setFormState({
+        ...formState,
+        [name]: value,
       });
-    }
-  }, [data, loading, dispatch]);
-
-  function filterProducts() {
-    if (!currentCategory) {
-      return state.products;
-    }
-
-    return state.products.filter(
-      (product) => product.category._id === currentCategory
-    );
-  }
+    };
+  
 
   return (
-    <div className="my-2">
-      <h2>Our Products:</h2>
-      {state.products.length ? (
-        <div className="flex-row">
-          {filterProducts().map((product) => (
-            <ProductItem
-              key={product._id}
-              _id={product._id}
-              image={product.image}
-              name={product.name}
-              price={product.price}
-              quantity={product.quantity}
-            />
-          ))}
-        </div>
-      ) : (
-        <h3>You haven't added any doggy gifts yet!</h3>
-      )}
-      {loading ? <img src={spinner} alt="loading" /> : null}
-    </div>
-  );
-}
+    <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
 
-export default ProductList;
+      <MDBTabs pills justify className='mb-3 d-flex flex-row justify-content-between'>
+        <MDBTabsItem>
+          <MDBTabsLink onClick={() => handleJustifyClick('tab1')} active={justifyActive === 'tab1'}>
+            Login
+          </MDBTabsLink>
+        </MDBTabsItem>
+        <MDBTabsItem>
+          <MDBTabsLink onClick={() => handleJustifyClick('tab2')} active={justifyActive === 'tab2'}>
+            Register
+          </MDBTabsLink>
+        </MDBTabsItem>
+      </MDBTabs>
+
+      <MDBTabsContent>
+
+        <MDBTabsPane show={justifyActive === 'tab1'}>
+
+
+
+          <MDBInput wrapperClass='mb-4' label='Email address' id='form1' type='email' onChange={handleChange}/>
+          <MDBInput wrapperClass='mb-4' label='Password' id='form2' type='password' onChange={handleChange}/>
+
+          <div className="d-flex justify-content-between mx-4 mb-4">
+            <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
+          </div>
+
+          <MDBBtn className="mb-4 w-100" onSubmit={handleFormLogin}>Sign in</MDBBtn>
+
+        </MDBTabsPane>
+
+        <MDBTabsPane show={justifyActive === 'tab2'}>
+
+      
+
+          <MDBInput wrapperClass='mb-4' name="firstName" label='First Name' id='form1' type='firstName' onChange={handleChange}/>
+          <MDBInput wrapperClass='mb-4' name="lastName" label='Last Name' id='form1' type='lastName' onChange={handleChange}/>
+          <MDBInput wrapperClass='mb-4' name="email" label='Email' id='form1' type='email' onChange={handleChange}/>
+          <MDBInput wrapperClass='mb-4' name="password" label='Password' id='form1' type='password' onChange={handleChange}/>
+
+          <div className='d-flex justify-content-center mb-4'>
+            <MDBCheckbox name='flexCheck' id='flexCheckDefault' label='I have read and agree to the terms' />
+          </div>
+
+          <MDBBtn className="mb-4 w-100" onSubmit={handleFormSignup}>Sign up</MDBBtn>
+
+        </MDBTabsPane>
+
+      </MDBTabsContent>
+
+    </MDBContainer>
+  );
+
+}
+  
+  export default Login;
