767828ed5213f8e9c337f93bbe6b9231cff5b8ea
Cuong Tran <nhoismee@gmail.com>
2024-04-09 14:33:36 -0700

    Reconfigured css and implemented donate page

--- client/src/components/Donate/Donate.js
+++ client/src/components/Donate/Donate.js
@@ -1,62 +1,42 @@
 import { useState, useEffect } from "react";
 import logo from '../../Assets/logo.png'
 import './Donate.css';
-import MainBody from '.. /MainBodyLayout/MainBody'
 import { MDBBtn, MDBInput } from 'mdb-react-ui-kit';
-
-// const [donate, setDonate] = useState('');
-
-// const handleInputChange = (e) => {
-//     const { target } = e;
-//     const inputType = target.name;
-//     const inputValue = target.value;
-
-//     if (inputType === 'donate') {
-//       setDonate(inputValue);
-//     };
-// };
+import SideNavbar from "../SideNavbar/SideNavbar"
 
 const ProductDisplay = ({price}) => (
-    <MainBody>
+      <div className="donate-wrapper">
         <section>
-        <div className="product">
-            <img
-            height="300px"
-            width="300px"
-            src={logo}
-            alt="The cover of Stubborn Attachments"
-            />
-            <div className="description">
-            <h3>Donate To The Doggos</h3>
-            <form className="form">
-                <label>Donate Here</label>
 
-                {/* <input
-                    defaultValue= {price}
-                    name="price"
-                    // onChange={handleInputChange}
-                    type="text"
-                    // placeholder="$10"
-                /> */}
-            </form>
-            </div>
-        </div>
-        <form className="button" action="http://localhost:3001/create-checkout-session" method="POST">
-            {/* <input type='hidden' name="price" value={price} /> */}
-            <MDBInput
-                    defaultValue= {price}
-                    name="price"
-                    // onChange={handleInputChange}
-                    type="text"
-                    // placeholder="$10"
-                />
-                <br />
-            <MDBBtn type="submit">
-            Pay Now
-            </MDBBtn>
-        </form>
+          <div className="product">
+              <img
+              height="300px"
+              width="300px"
+              src={logo}
+              alt="The cover of Stubborn Attachments"
+              />
+              <div className="description">
+              <h3>Donate To The Doggos</h3>
+              <form className="form">
+                  <label>Donate Here</label>
+              </form>
+              </div>
+          </div>
+
+          <form className="button" action="http://localhost:3001/create-checkout-session" method="POST">
+              <MDBInput
+                      defaultValue= {price}
+                      name="price"
+                      type="text"
+                  />
+                  <br />
+              <MDBBtn type="submit">
+              Pay Now
+              </MDBBtn>
+          </form>
+
         </section>
-    </MainBody>
+      </div>
   );
   
   const Message = ({ message }) => (
@@ -73,7 +53,7 @@ export const Donate = () => {
         useEffect(() => {
           // Check to see if this is a redirect back from Checkout
           const query = new URLSearchParams(window.location.search);
-      
+
           if (query.get("success")) {
             setMessage("Order placed! You will receive an email confirmation.");
           }
@@ -90,6 +70,7 @@ export const Donate = () => {
         ) : (
           <ProductDisplay price={price} />
         );
+        
 }
 
 export default Donate;
