38bda3d6f30f61aa59b28029e39286b4d5c8ed2a
RishiMannava <90156867+Wyldn@users.noreply.github.com>
2024-04-10 01:07:29 -0700

    made the profile page more intuitive for capturing input fields for updates

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,117 @@
-import React from 'react';
-import './Home.css';
-import SideNavbar from '../../components/SideNavbar/SideNavbar';
-import CardFeed from '../../components/CardFeed/CardFeed';
+import { gql } from '@apollo/client';
 
-export const Landing = () => {
-  return (
-    <>
-        <SideNavbar />
-        <CardFeed />
-    </>
-  );
-};
+export const LOGIN = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
 
-export default Landing;
+export const ADD_USER = gql`
+  mutation addUser(
+    $firstName: String!
+    $lastName: String!
+    $email: String!
+    $password: String!
+    $dogName: String!
+    $gender: String!
+    $breed: String!
+    $birthday: String!
+  ) {
+    addUser(
+      firstName: $firstName
+      lastName: $lastName
+      email: $email
+      password: $password
+      dogName: $dogName
+      gender: $gender
+      breed: $breed
+      birthday: $birthday
+    ) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
+
+export const UPDATE_USER = gql`
+  mutation updateUser(
+    $firstName: String
+    $lastName: String
+    $email: String
+    $dogName: String
+    $gender: String
+    $breed: String
+    $birthday: String
+  ) {
+    updateUser(
+      firstName: $firstName
+      lastName: $lastName
+      email: $email
+      dogName: $dogName
+      gender: $gender
+      breed: $breed
+      birthday: $birthday
+    )  {
+      _id
+      birthday
+      breed
+      dogName
+      email
+      firstName
+      gender
+      lastName
+    }
+  }
+`;
+
+// export const ADD_DOG = gql`
+//   mutation addDog(
+//     $dogName: String!
+//     $gender: String!
+//     $breed: String!
+//     $birthday: String!
+//     $preferences: String
+//   ) {
+//     addDog(
+//       dogName: $dogName
+//       gender: $gender
+//       breed: $breed
+//       birthday: $birthday
+//       preferences: $preferences
+//     ) {
+//       dogName
+//       gender
+//       breed
+//       birthday
+//     }
+//   }
+// `;
+
+// export const UPDATE_DOG = gql`
+//   mutation updateDog(
+//     $dogName: String
+//     $profilePicture: String
+//     $pictures: String
+//     $gender: String
+//     $breed: String
+//     $birthday: String
+//     $preferences: String
+//   ) {
+//     updateDog(
+//       dogName: $dogName
+//       profilePicture: $profilePicture
+//       pictures: $pictures
+//       gender: $gender
+//       breed: $breed
+//       birthday: $birthday
+//       preferences: $preferences
+//     )
+//   }
+// `;
