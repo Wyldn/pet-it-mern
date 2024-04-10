be199e1145a41fb565b35101b23c44e5bcc2ecea
RishiMannava <90156867+Wyldn@users.noreply.github.com>
2024-04-10 01:30:34 -0700

    made the donate page more user friendly based on logging in status

---
+++ client/src/pages/Profile/Profile.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './Profile.css';
+import Profile from '../../components/Profile/Profile';
+import SideNavbar from '../../components/SideNavbar/SideNavbar';
+
+
+
+export const Landing = () => {
+  return (
+    <>
+        <SideNavbar />
+        <Profile />
+    </>
+  );
+};
+
+export default Landing;
